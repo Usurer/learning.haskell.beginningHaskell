8080b806cc117b5a25706e7acd089fc7e98ad385
Usurer <ivvlg@narod.ru>
2017-12-29 14:31:20 +0300

    Initial commit with some data

---
+++ Chapter2/SimpleFunctions.hs
@@ -0,0 +1,43 @@
+module SimpleFunctions where
+
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n-2) + fibonacci (n-1)
+
+data Client 	= GovOrg String
+		| Company String Integer String String
+		| Individual Person
+		deriving Show
+
+data Person = Person String String Gender deriving Show
+
+data Gender = Male | Female | Unknown deriving Show
+
+data Manufacturer = Manufacturer String
+data TravelDirection = Past | Future | Both
+data TimeMachine = TimeMachine Manufacturer Integer String TravelDirection Float
+
+clientName :: Client -> Maybe String
+clientName client = case client of 
+	GovOrg name -> Nothing
+	Company name _ _ _ -> Nothing
+	Individual (Person fName lName _) -> Just (fName ++ " " ++ lName)
+
+companyName client = case client of 
+	Company name _ _ _ -> Just name
+	_ -> Nothing
+
+genderDiversion :: [Client] -> (Integer, Integer)
+genderDiversion lst  = if null lst
+	then (0, 0)
+	else if null (tail lst)
+		then d
+		else (fst d + males, snd d + females)
+	where 	males = fst (genderDiversion (tail lst))
+		females = snd (genderDiversion (tail lst))
+		d = case (head (lst)) of
+			Individual (Person _ _ gender) -> case gender of
+				Male -> (1, 0 )
+				Female -> (0, 1)
+				_ -> (0, 0)
+			_ -> (0, 0)
