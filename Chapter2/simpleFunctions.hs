188e9e8d9a06746fc4051839260bcea1b9cb9758
Usurer <ivvlg@narod.ru>
2018-02-15 17:32:00 +0300

    Typo fixed. MaxMin function added

--- Chapter2/simpleFunctions.hs
+++ Chapter2/simpleFunctions.hs
@@ -8,4 +8,13 @@ lst1 +++ lst2 = if null lst1
 reverse2 :: [x] -> [x]
 reverse2 lst = if null lst
     then []
-    else reverse2 (tail lst) +++ (head lst]
+    else reverse2 (tail lst) +++ [head lst]
+
+maxmin :: Ord a => [a] -> (a, a)
+maxmin lst = if null (tail lst)
+    then (head lst, head lst)
+    else if head lst > fst (maxmin (tail lst))
+        then (head lst, snd (maxmin (tail lst)))
+        else if head lst < snd (maxmin (tail lst))
+            then (fst (maxmin (tail lst)), head lst)
+            else maxmin (tail lst)
