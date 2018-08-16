8ea2cc45310213cab4eb21208e21bddb6c12b072
Usurer <ivvlg@narod.ru>
2018-08-16 20:10:45 +0300

    Starting all shit anew =\

--- Chapter2/SimpleFunctions.hs
+++ Chapter2/SimpleFunctions.hs
@@ -1,56 +1,27 @@
-module SimpleFunctions where
+module Chapter2.SimpleFunctions where
 
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = fibonacci (n-2) + fibonacci (n-1)
+firstOrEmpty :: [[Char]] -> [Char]
+firstOrEmpty lst = if not (null lst) then head lst else "empty"
 
-data Client 	= GovOrg String
-		| Company String Integer String String
-		| Individual Person
-		deriving Show
+(+++) :: [a] -> [a] -> [a]
+lst1 +++ lst2 = if null lst1
+		then lst2
+		else (head lst1) : (tail lst1 +++ lst2)
 
-data Person = Person String String Gender deriving Show
+reverse2 :: [a] -> [a]
+reverse2 lst = if null lst
+	then lst
+	else reverse2 (tail lst) +++ [head lst]
 
-data Gender = Male | Female | Unknown deriving Show
+maxmin :: Ord a => [a] -> (a, a)
+maxmin lst = if null (tail lst)
+	then (head lst, head lst)
+	else (	if fst (maxmin (tail lst)) > head lst
+		then fst (maxmin (tail lst) )
+		else head lst,
+		if snd (maxmin (tail lst)) < head lst
+		then snd (maxmin (tail lst))
+		else head lst
+	     )
 
-data Manufacturer = Manufacturer String deriving Show
-data TravelDirection = Past | Future | Both deriving Show
-data TimeMachine = TimeMachine { manufacturer :: Manufacturer
-	, model :: Integer
-	, name :: String
-	, direction :: TravelDirection
-	, price :: Float }
-	deriving Show
-
-clientName :: Client -> Maybe String
-clientName client = case client of 
-	GovOrg name -> Nothing
-	Company name _ _ _ -> Nothing
-	Individual (Person fName lName _) -> Just (fName ++ " " ++ lName)
-
-companyName client = case client of 
-	Company name _ _ _ -> Just name
-	_ -> Nothing
-
-genderDiversion :: [Client] -> (Integer, Integer)
-genderDiversion lst  = if null lst
-	then (0, 0)
-	else if null (tail lst)
-		then d
-		else (fst d + males, snd d + females)
-	where 	males = fst (genderDiversion (tail lst))
-		females = snd (genderDiversion (tail lst))
-		d = case (head (lst)) of
-			Individual (Person _ _ gender) -> case gender of
-				Male -> (1, 0 )
-				Female -> (0, 1)
-				_ -> (0, 0)
-			_ -> (0, 0)
-
-salePrices :: [TimeMachine] -> [TimeMachine]
-salePrices [x@(TimeMachine { price = p })] = [(x { price = (p / 2) })]
-salePrices lst = case lst of
-	[x@(TimeMachine { price = p })] -> [(x { price = (p / 2) })]
-	x:xs -> (salePrices [x]) ++ (salePrices xs)
-	[] -> []
-
+		
