ae136ab86cbed3920a30910b01d312625141a405
Usurer <ivvlg@narod.ru>
2018-08-17 04:17:41 +0300

    Basic ADT added

--- Chapter2/AlgebraicDataTypes.hs
+++ Chapter2/AlgebraicDataTypes.hs
@@ -1,3 +1,17 @@
 module AlgebraicDataTypes where
 
+data Client = GovOrg String
+            | Company String Integer String String
+            | Individual Person Bool
+              deriving Show
 
+data Person = Person String String Gender
+              deriving Show
+
+data Gender = Male | Female | Unknown deriving Show
+
+data Manufacturer = Manufacturer String
+
+data TimeTravelDirection = Future | Past | Both
+
+data TimeMachine = TimeMachine Manufacturer Integer String TimeTravelDirection Float
