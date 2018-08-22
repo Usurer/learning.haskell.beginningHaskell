b2711994bb38f7cefc3655ea2dea26a4c8dc1b4d
Usurer <ivvlg@narod.ru>
2018-08-22 16:56:39 +0300

    Ex 2.5: Clients gender refactored

--- Chapter2/Ex2_5.hs
+++ Chapter2/Ex2_5.hs
@@ -8,18 +8,29 @@ clientGender client = case client of
     _                                -> Nothing 
 
 clientsGender :: [Client] -> (Integer, Integer)
-clientsGender lst = let h = head lst in 
-    if null (tail lst)
-    then case clientGender h of 
-        Just n -> case n of
-                    Male -> (1, 0)
-                    Female -> (0, 1)
-                    _   -> (0, 0)
-    else case clientGender h of
-        Just n -> case n of
-                    Male -> (males + 1, females)
-                    Female -> (males, females + 1)
-                    _       -> (males, females)
-    where recurse = clientsGender (tail lst)
-          males = fst recurse
-          females = snd recurse
+
+clientsGender [] = (0, 0)
+
+clientsGender [a] = let res = clientGender a in
+    case res of
+        Nothing -> (0, 0)
+        Just val -> case val of
+            Male -> (1, 0)
+            Female -> (0, 1)
+            _      -> (0, 0)
+
+clientsGender lst = let h = clientsGender [(head lst)] in
+    (fst h + males, snd h + females)
+    where males = fst (clientsGender (tail lst))
+          females = snd (clientsGender (tail lst))
+
+
+testClient_1 = Individual (Person "Bob" "Bobson" Male) False
+testClient_2 = Individual (Person "John" "Jonhsson" Male) False
+testClient_3 = Individual (Person "Alice" "Alicedottir" Female) False
+testClient_4 = Individual (Person "Jane" "Smith" Female) False
+testClient_5 = Individual (Person "Lou" "Black" Unknown) False
+testClient_6 = Individual (Person "Jack" "Smith" Male) False
+testClient_7 = GovOrg "NASA"
+testList = [testClient_1,testClient_2,testClient_3,testClient_4,testClient_5,testClient_6,testClient_7]
+
