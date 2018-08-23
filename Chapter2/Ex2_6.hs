5050000aa6192c8a28ffe54bedb5e159d468f32c
Usurer <ivvlg@narod.ru>
2018-08-23 23:49:47 +0300

    Excercise 2.6: Ackermann function

---
+++ Chapter2/Ex2_6.hs
@@ -0,0 +1,17 @@
+module Ex2_6 where
+
+{-
+    An attempt to execute the current implementation of the
+    Ackermann function will lead to terrible memory consumption
+    and unfinished process (never gonna finish, should be killed).
+    According to SO 
+        https://stackoverflow.com/questions/16115815/ackermann-very-inefficient-with-haskell-ghc
+    this is an issue of GHC.
+-}
+
+ackermann :: Integer -> Integer -> Integer
+ackermann m n | m == 0 = n + 1
+ackermann m n | m > 0 && n == 0 = ackermann (m - 1) 1
+ackermann m n | m > 0 && n > 0 = ackermann (m - 1) (ackermann m (n - 1))
+
+
